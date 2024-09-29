6e8339516d26ed15fb2dab821c9fc846e8a682c9
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-09-29 11:20:56 +0200

    run format

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "rss-devops-course-terraform"
-    key            = "terraform/state/terraform.tfstate"
-    region         = "eu-west-1"
+    bucket = "rss-devops-course-terraform"
+    key    = "terraform/state/terraform.tfstate"
+    region = "eu-west-1"
   }
 }
